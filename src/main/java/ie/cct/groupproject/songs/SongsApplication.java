4984e89497213c4b0dfe822a922fa08ecc8fa256
Bekezhan <72031152+Bekardi@users.noreply.github.com>
2023-04-25 18:43:19 +0100

    Initial commit

---
+++ src/main/java/ie/cct/groupproject/songs/SongsApplication.java
@@ -0,0 +1,13 @@
+package ie.cct.groupproject.songs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongsApplication.class, args);
+	}
+
+}
